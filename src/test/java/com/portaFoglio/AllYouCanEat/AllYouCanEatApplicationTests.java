5e3246a2a1d338d48036e96ced5a9a666d8b83f0
MaxmosRefky <m.abdelsayed@contrader.it>
2023-12-27 17:38:14 +0100

    Initial commit

---
+++ src/test/java/com/portaFoglio/AllYouCanEat/AllYouCanEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portaFoglio.AllYouCanEat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllYouCanEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
