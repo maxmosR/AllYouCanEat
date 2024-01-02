b696dd4428ed7e742e32af8c591c1f585611f00c
EuMarinelli <eugenio.marinelli900@gmail.com>
2024-01-02 10:28:29 +0100

    Aggiunta class OrderAddress.

---
+++ src/main/java/com/portafoglio/allyoucaneat/model/OrderAddress.java
@@ -0,0 +1,30 @@
+package com.portafoglio.allyoucaneat.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "order_address")
+public class OrderAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @OneToOne(mappedBy = "address", cascade = CascadeType.ALL)
+    private Cart cart;
+}
