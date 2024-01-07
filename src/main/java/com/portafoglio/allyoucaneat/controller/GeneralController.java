d21c8d0e9eee2c8797ba375f29aa33749229d812
MaxmosRefky <maxmos.abdelsayed@gmail.com>
2024-01-07 19:54:26 +0100

    login google and facebook

--- src/main/java/com/portafoglio/allyoucaneat/controller/GeneralController.java
+++ src/main/java/com/portafoglio/allyoucaneat/controller/GeneralController.java
@@ -1,13 +1,22 @@
 package com.portafoglio.allyoucaneat.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class GeneralController {
 
-    @GetMapping
+    @GetMapping("/string")
     String stringaConsol(){
         return "maxitos";
+    }
+    @GetMapping("googlelogin")
+    public Map<String, Object> currentUser(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        return oAuth2AuthenticationToken.getPrincipal().getAttributes();
     }
 }
