d721843d252826b7a77466b80df1f8b36a006d42
MaxmosRefky <maxmos.abdelsayed@gmail.com>
2024-01-08 11:19:39 +0100

    Update GeneralController.java

--- src/main/java/com/portafoglio/allyoucaneat/controller/GeneralController.java
+++ src/main/java/com/portafoglio/allyoucaneat/controller/GeneralController.java
@@ -1,14 +1,14 @@
 package com.portafoglio.allyoucaneat.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/logininfo")
 public class GeneralController {
 
     @GetMapping("/string")
